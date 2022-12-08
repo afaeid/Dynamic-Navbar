c16bc7636b26c6df01586ca8c17accca50ac6d76
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-12-08 22:23:43 +0600

    ok

---
+++ main.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('section');
+const navManue = document.querySelectorAll('nav ul li a');
+
+
+  window.addEventListener('scroll', ()=>{
+      let current = '';
+sections.forEach((section) => {
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+   if(pageYOffset >= sectionTop - (sectionHeight/1.5)){
+     current = section.getAttribute('id');
+   }
+  
+  })
+ navManue.forEach((li)=> {
+ li.classList.remove('active')
+   if(li.classList.contains(current)){
+   li.classList.add('active');
+   }
+ })
+  
+})
